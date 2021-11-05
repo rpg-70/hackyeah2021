cddbbe4f561604694c8763cd4f328ec3f845151b
Piotr Guziorski <piotr.guziorski@gmail.com>
2021-11-05 21:28:09 +0100

    Added swagger ui

--- src/main/java/org/hackyeah/VersionController.java
+++ src/main/java/org/hackyeah/VersionController.java
@@ -1,5 +1,6 @@
 package org.hackyeah;
 
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.MediaType;
@@ -28,8 +29,7 @@ public class VersionController {
 
     @GetMapping(value = "/", produces = MediaType.TEXT_HTML_VALUE)
     public String splash() throws IOException {
-        byte[] bytes = getClass().getResourceAsStream("/img/waiting.gif").readAllBytes();
-        String imageData = Base64.getEncoder().encodeToString(bytes);
+        String imageData = Base64.getEncoder().encodeToString(IOUtils.toByteArray(getClass().getResourceAsStream("/img/waiting.gif")));
 
         return body(
                 h1("Ready for HackYeah 2021? ;-)"),
