fe547d42e73548a0da748000b83aa42f8ae861da
Karol <rpg-7@o2.pl>
2021-12-11 22:02:00 +0100

    cors config

--- src/main/java/com/bikpicture/app/scoring/ScoringController.java
+++ src/main/java/com/bikpicture/app/scoring/ScoringController.java
@@ -8,10 +8,7 @@ import feign.FeignException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.util.AbstractMap;
@@ -22,6 +19,7 @@ import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping(path = "/scoring")
 @RequiredArgsConstructor
 @Slf4j
